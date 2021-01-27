485b9fb1f583a278d32c689840d8ae699625d81d
Aleksei Sidorin <sidorinaleksei@gmail.com>
2021-01-27 04:18:59 +0300

    add method of findMaxIndex

--- src/main/java/StatisticsService.java
+++ src/main/java/StatisticsService.java
@@ -8,11 +8,23 @@ public class StatisticsService {
      * @return - index of first max value
      */
     public long findMax(long[] incomes) {
-        long current_max_index = 0;
+        //long current_max_index = 0;
         long current_max = incomes[0];
         for (long income : incomes)
             if (current_max < income)
                 current_max = income;
         return current_max;
+    }
+
+    public long findMaxIndex(long[] incomes) {
+        long current_max_index = 0;
+        long current_max = incomes[0];
+        for (long income : incomes) {
+            if (current_max < income) {
+                current_max = income;
+                current_max_index++;
+            }
+        }
+        return current_max_index;
     }
 }
--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -7,27 +7,30 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StatisticsServiceTest {
 
+    StatisticsService service = new StatisticsService();
+
     @Test
     void findMax() {
-        StatisticsService service = new StatisticsService();
-
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
-
         long actual = service.findMax(incomesInBillions);
-
         assertEquals(expected, actual);
     }
 
     @Test
     void findNewMax() {
-        StatisticsService service = new StatisticsService();
-
         long[] incomesInBillions = {12, 5, 34, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 34;
-
         long actual = service.findMax(incomesInBillions);
-
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxIndex() {
+        long[] incomesInBillions = {12, 5, 34, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 1;
+        long actual = service.findMaxIndex(incomesInBillions);
+        assertEquals(expected, actual);
+
     }
 }
